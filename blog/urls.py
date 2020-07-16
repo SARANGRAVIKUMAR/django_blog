d1bca305783a654c99f3b807b7333d6ee52cf885
sarang ravikumar <sarangravikumar1999@gmail.com>
2020-07-16 22:48:20 +0530

    CRUD operation in blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="blog-home"),
-    path('about/',views.about,name="blog-about"),
+    # path('',views.home,name="blog-home"),
+    path('', views.PostListView.as_view(template_name='home.html'), name="blog-home"),
+    path('about/', views.about, name="blog-about"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(template_name='post_forms.html'), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(template_name='post_forms.html'), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(template_name="post_confirm_delete.html"),
+         name='post-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+'''login required mixin is used so that we will able to access the page only if its logged in 
+User passes test mixin is used for checking some test condition for users to visit a certain page 
+in here only the author who have written the blog can update it '''
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 
@@ -13,3 +18,57 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    '''to make the ordering from newest to oldest'''
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    '''overding the form during submision so that new blog will be created on any of the author '''
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    '''overding the form during submision so that new blog will be created on any of the author '''
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    '''test_func is a boolean function based on the return value we will be able to update the post'''
+
+    def test_func(self):
+        ''' will return the post that we r curently going to acces'''
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
